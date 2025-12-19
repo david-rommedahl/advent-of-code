6f149bfb5fcafa121fe6127c66868e830017b425
David Rommedahl <david.rommedahl@ericsson.com>
2025-12-19 14:11:14 -0500

    Added ray-cast function and area functions to part 2

--- 2025/9/part_2.py
+++ 2025/9/part_2.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+from functools import reduce
+from itertools import combinations
 from pathlib import Path
 from time import perf_counter
 
@@ -32,11 +34,50 @@ def is_on_border(point: tuple[int, int]) -> bool:
     return False
 
 
+def ray_cast(point: tuple[int, int]) -> bool:
+    """Counts how many borders a ray starting at a point and going straight up would cross."""
+    x, y = point
+    rows_above = [rows[row] for row in rows if row < y]
+    num_intersections = 0
+    for row in rows_above:
+        line_segments = [segment for i in range(0, len(row), 2) if (segment := row[i : i + 2])]
+        for start, end in line_segments:
+            if start < x < end:
+                num_intersections += 1
+    return num_intersections % 2 == 1
+
+
+def check_point(point: tuple[int, int]) -> bool:
+    return is_on_border(point) or ray_cast(point)
+
+
+def length(*coords):
+    if len(coords) != 2:
+        raise ValueError("Need two values")
+    return abs(reduce(lambda a, b: a - b, coords)) + 1
+
+
+def area(a, b):
+    x_coords, y_coords = zip(a, b)
+    x_length = length(*x_coords)
+    y_length = length(*y_coords)
+
+    return x_length * y_length
+
+
 for x, y in tile_coordinates:
     rows[y].append(x)
     columns[x].append(y)
 
-print("Number of points in rows: ", set(len(row) for row in rows.values()))
-print("Number of points in columns: ", set(len(column) for column in columns.values()))
+point_combinations = combinations(tile_coordinates, 2)
+valid_combinations = []
+for comb in point_combinations:
+    a, b = comb
+    c_d = (a[0], b[1]), (b[0], a[1])
+    if all(check_point(p) for p in c_d):
+        valid_combinations.append(comb)
+area_comb = [(x, area(*x)) for x in valid_combinations]
+max_area = max(area_comb, key=lambda x: x[1])
 t2 = perf_counter()
+print("Max area: ", max_area)
 print(f"Total time: {t2 - t1:.4f}")
